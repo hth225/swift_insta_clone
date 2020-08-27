457e8195c837788ba756bd589af6ab5d8919d92e
Taeho Hwang <hth225@gmail.com>
2020-08-27 13:44:11 +0900

    Add image picker which interact with photo library(UIkit)

--- instagram_clone/SignupView.swift
+++ instagram_clone/SignupView.swift
@@ -12,20 +12,36 @@ struct SignupView: View {
     @State var username : String = ""
     @State var email : String = ""
     @State var password : String = ""
+    @State var showImagePicker : Bool = false
+    @State var image : Image = Image(IMAGE_USER_PLACEHOLDER)
+    @State var imageData : Data = Data()
+    func signUp() {
+//        Firebase.createAccount(username: username, email: email, password: password, imageData: imageData)
+    }
     
     var body: some View {
         VStack {
-            Image(IMAGE_USER_PLACEHOLDER).resizable().aspectRatio(contentMode: .fill).frame(width: 50, height: 50).clipShape(Circle()).padding(.bottom, 80)
+            image.resizable().aspectRatio(contentMode: .fill)
+                .frame(width: 50, height: 50)
+                .clipShape(Circle()).padding(.bottom, 80)
+                .onTapGesture {
+                    self.showImagePicker = true
+            }
             UserNameTextField(username: $username)
             EmailTextField(email: $email)
             VStack(alignment: .leading) {
                 PasswordTextField(password: $password)
                 Text(TEXT_SIGNUP_PASSWORD_REQUIRED).font(.footnote).foregroundColor(.gray).padding(.leading)
             }
-            SignupButton(actoin: {}, label: TEXT_SIGN_UP)
+            SignupButton(actoin: signUp, label: TEXT_SIGN_UP)
             Divider()
             Text(TEXT_SIGNUP_NOTE).font(.footnote).foregroundColor(.gray).padding().lineLimit(2)
-        }.navigationBarTitle("Register", displayMode: .inline)
+            }
+        .sheet(isPresented: $showImagePicker) {
+//            ImagePickerController()
+            ImagePicker(showImagePicker: self.$showImagePicker, pickedImage: self.$image, imageData: self.$imageData)
+        }
+        .navigationBarTitle("Register", displayMode: .inline)
     }
 }
 
